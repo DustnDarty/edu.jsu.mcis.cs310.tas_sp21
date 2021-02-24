2c26b993cec9a09ea8fd28b75055afce8eb31909
fox8091 <bfox200012@gmail.com>
2021-02-24 00:27:03 -0600

    Implement Badge, remove boilerplate from Punch and Shift

--- src/edu/jsu/mcis/cs310/tas_sp21/Badge.java
+++ src/edu/jsu/mcis/cs310/tas_sp21/Badge.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.jsu.mcis.cs310.tas_sp21;
 
-/**
- *
- * @author Bailey Fox <bfox200012@gmail.com>
- */
 public class Badge {
+    private String id;
+    private String description;
+
+    public Badge(String id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
     
+    @Override
+    public String toString(){
+        StringBuilder output = new StringBuilder();
+        output.append("#").append(this.id).append(" ");
+        output.append("(").append(this.description).append(")");
+        
+        return output.toString();
+    }
 }
