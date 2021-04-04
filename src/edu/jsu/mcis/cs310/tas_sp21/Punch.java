10e78f74e4501a5e8b6ac67da8d896f5c7930c38
Tiffany O'Dell <jsu3583m@stu.jsu.edu>
2021-04-04 09:41:12 -0500

    adjust() - fails 2nd assertEquals() in testAdjustPunchesShift1Weekend()

--- src/edu/jsu/mcis/cs310/tas_sp21/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_sp21/Punch.java
@@ -3,9 +3,12 @@ package edu.jsu.mcis.cs310.tas_sp21;
 //import java.util.GregorianCalendar;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.Date;
 import java.util.Locale;
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 
 public class Punch {
     
@@ -95,129 +98,123 @@ public class Punch {
     public void adjust(Shift s){
         
         Date date = new Date(originaltimestamp);
-
+        
+        // Get name of day
         SimpleDateFormat formatter = new SimpleDateFormat("EEE");
-        String strDate = formatter.format(date);
+        String strDate = formatter.format(date).toUpperCase();
+        
+        //convert originaltimestamp from long to LocalTime object
+        LocalTime lt = Instant.ofEpochMilli(originaltimestamp).atZone(ZoneId.systemDefault()).toLocalTime();
         
         switch(this.punchtypeid){
-            case 0: //punch out
-                //insert code
-                //shift stop
-                if( strDate != "SAT" && strDate != "SUN" ){
-                    if( s.getStop().toSecondOfDay() * 1000 < originaltimestamp && originaltimestamp < s.getStop().plusMinutes(s.getInterval()).toSecondOfDay() * 1000 ){
-                    adjustedtimestamp = s.getStop().toSecondOfDay() * 1000;
-                    adjustmenttype = "Shift Stop";
+            /************************* PUNCH OUT *************************/
+            case 0: 
+                // On Weekdays
+                if(strDate != "SAT" && strDate != "SUN" ){
+                    //LATE CLOCK-OUT: SHIFT STOP within 15-min interval (ADJUST LATE CLOCK-OUT BACKWARD TO THE SCHEDULED END OF THE SHIFT)
+                    if ( lt.isAfter(s.getStop()) && (Math.abs(s.getStop().toSecondOfDay() - lt.toSecondOfDay()) <= s.getInterval() * 60)){
+                        adjustedtimestamp = originaltimestamp - 1000 * Math.abs(lt.toSecondOfDay() - (s.getStop().toSecondOfDay())); 
+                        adjustmenttype = "Shift Stop";
+                   }
+                    // LATE LUNCH START (clock OUT punch WITHIN the lunch break is realigned with the START of lunch break
+                    else if (lt.isAfter(s.getLunchStart())  &&  lt.isBefore(s.getLunchStop())){
+                        adjustedtimestamp = originaltimestamp - 1000 * Math.abs(lt.toSecondOfDay() - s.getLunchStart().toSecondOfDay()); 
+                        adjustmenttype = "Lunch Start";
+                        System.err.println("In Lunch Start (should not include SAT!!): " + strDate);
+                    }  
+                    // EARLY CLOCK-OUT - GRACE PERIOD - 5 min (EARLY CLOCK OUT punch within GRACE PERIOD before END of shift is realigned with the STOPPING time of the shift)
+                    else if (lt.isBefore(s.getStop()) && lt.isAfter(s.getLunchStop()) && (Math.abs(s.getStop().toSecondOfDay() - lt.toSecondOfDay()))  <= s.getGracePeriod() * 60){
+                        adjustedtimestamp = originaltimestamp + 1000 * Math.abs(s.getStop().toSecondOfDay() - lt.toSecondOfDay()); 
+                        adjustmenttype = "Shift Grace";
+                    }
+                    // DOCK 15 min (CLOCK OUT too early to fall within grace period; adjust punch BACKWARD in time from end of shift
+                    else if( lt.isBefore(s.getStop()) && lt.isAfter(s.getLunchStop()) && (Math.abs(s.getStop().toSecondOfDay() - lt.toSecondOfDay())) > s.getGracePeriod() * 60 && (Math.abs(s.getStop().toSecondOfDay() - lt.toSecondOfDay()) <= s.getDock() * 60) )
+                    {
+                        adjustedtimestamp = (originaltimestamp + (Math.abs(lt.toSecondOfDay() - s.getStop().toSecondOfDay())) * 1000) - (s.getDock() * 60 * 1000); 
+                        adjustmenttype = "Shift Dock"; 
+                    }
+                    // none: (if punch occurred at even time interval, no adjustment needed; reset seconds to zero
+                    else if ((lt.getMinute() % s.getInterval()) == 0 ){
+                        adjustedtimestamp = originaltimestamp - (lt.getSecond() * 1000);
+                        adjustmenttype = "None";
+                    } 
+                    // INTERVAL ROUNDING applies only if OUTSIDE any of the Shift Rules 
+                    else{
+                     intervalRound(lt, s);
                     }
                 }
-                
-                //lunch start
-                if( strDate != "SAT" && strDate != "SUN" ){
-                    if( s.getLunchStart().toSecondOfDay() * 1000 < originaltimestamp && originaltimestamp < s.getLunchStop().toSecondOfDay() * 1000 ){
-                    adjustedtimestamp = s.getLunchStart().toSecondOfDay() * 1000;
-                    adjustmenttype = "Lunch Start";
-                    }   
-                }
-                 
-                
-                //interval
-                if( strDate == "SAT" || strDate == "SUN" )
-                if( s.getStop().plusMinutes(s.getInterval()).toSecondOfDay() * 1000 < originaltimestamp ){
-                    if( s.getStop().plusMinutes(s.getGracePeriod()).toSecondOfDay() * 1000 < originaltimestamp && s.getStop().plusMinutes(s.getDock()).toSecondOfDay() * 1000 < originaltimestamp ){
-                        if( originaltimestamp < s.getLunchStart().toSecondOfDay() * 1000 && s.getLunchStop().toSecondOfDay() * 1000 < originaltimestamp ){
-                            if( s.getStop().plusMinutes(s.getInterval()).toSecondOfDay() * 1000 < originaltimestamp){
-                                adjustedtimestamp = originaltimestamp;
-                            }
-                            adjustmenttype = "Interval Round";
-                        }
-                    }       // UNFINISHED
-                }
-                
-                // graceperiod
-                if( strDate != "SAT" && strDate != "SUN" ){
-                    if( s.getStop().minusMinutes(s.getGracePeriod()).toSecondOfDay() * 1000 < originaltimestamp && originaltimestamp < s.getStop().toSecondOfDay() * 1000 ){
-                    adjustedtimestamp = s.getStop().toSecondOfDay() * 1000;
-                    adjustmenttype = "Grace Period";
-                    }
-                }
-                
-                // dock
-                if( strDate != "SAT" && strDate != "SUN" ){
-                    if( s.getStop().minusMinutes(s.getDock()).toSecondOfDay() * 1000 < originaltimestamp && originaltimestamp < s.getStop().minusMinutes(s.getGracePeriod()).toSecondOfDay() * 1000 ){
-                    adjustedtimestamp = originaltimestamp - (s.getDock() * 60000);
-                    adjustmenttype = "Dock";
-                    }
-                }
-                
-                // none
-                if( strDate != "SAT" && strDate != "SUN" ){
-                    if( s.getStop().minusMinutes(1).toSecondOfDay() * 1000 < originaltimestamp && originaltimestamp < s.getStop().plusMinutes(1).toSecondOfDay() * 1000  ){
-                    adjustedtimestamp = s.getStop().toSecondOfDay() * 1000;
-                    adjustmenttype = "None";
-                    }
+                // WEEKEND REQUIRES ONLY INTERVAL ROUNDING
+                else if ( strDate == "SAT" || strDate == "SUN" ){
+                     intervalRound(lt, s);
                 }
                 break;
-            
-            case 1: // punch in
-                // insert code
-                // shift start
+             
+            /************************* PUNCH IN *************************/
+            case 1: 
+                // On Weekdays
                 if( strDate != "SAT" && strDate != "SUN" ){
-                    if( s.getStart().minusMinutes(s.getInterval()).toSecondOfDay() * 1000 < originaltimestamp && originaltimestamp < s.getStart().toSecondOfDay() * 1000 ){
-                    adjustedtimestamp = s.getStart().toSecondOfDay() * 1000;
-                    adjustmenttype = "Shift Start";
+                     // EARLY CLOCK IN - SHIFT START -  15 min interval (adjust EARLY clock-in FORWARD)
+                    if ( lt.isBefore(s.getStart()) && (Math.abs(s.getStart().toSecondOfDay() - lt.toSecondOfDay())  <= s.getInterval() * 60)){
+                       adjustedtimestamp = originaltimestamp + 1000 * Math.abs(s.getStart().toSecondOfDay() - lt.toSecondOfDay());
+                       adjustmenttype = "Shift Start";
                     }
-                }
-                
-                // lunch stop
-                if( strDate != "SAT" && strDate != "SUN" ){
-                    if( s.getLunchStart().toSecondOfDay() * 1000 < originaltimestamp && originaltimestamp < s.getLunchStop().toSecondOfDay() * 1000 ){
-                    adjustedtimestamp = s.getStop().toSecondOfDay() * 1000;
-                    adjustmenttype = "Lunch Stop";
+                    // LUNCH STOP (the CLOCK IN punch WITHIN the lunch break realigns with the STOP of lunch break
+                    else if (lt.isAfter(s.getLunchStart())  &&  lt.isBefore(s.getLunchStop())){
+                       adjustedtimestamp = originaltimestamp + 1000 * Math.abs(s.getLunchStop().toSecondOfDay() - lt.toSecondOfDay()); 
+                       adjustmenttype = "Lunch Stop";
+                    }  
+                    // LATE CLOCK IN - GRACE PERIOD - 5 min (LATE CLOCK IN within grace period AFTER start of shift realigns with STARTING time of the shift)
+                    else if ( lt.isAfter(s.getStart()) && lt.isBefore(s.getLunchStart()) && ((Math.abs( lt.toSecondOfDay() - s.getStart().toSecondOfDay())  <= s.getGracePeriod() * 60))){
+                       adjustedtimestamp = originaltimestamp - 1000 * Math.abs(lt.toSecondOfDay() - s.getStart().toSecondOfDay()); 
+                       adjustmenttype = "Shift Grace";
                     }
-                }
-                
-                // interval
-                if( strDate == "SAT" || strDate == "SUN" ){
-                    if( originaltimestamp < s.getStart().minusMinutes(s.getInterval()).toSecondOfDay() * 1000 ){
-                        if( originaltimestamp <  s.getStart().minusMinutes(s.getGracePeriod()).toSecondOfDay() * 1000  && originaltimestamp < s.getStart().minusMinutes(s.getDock()).toSecondOfDay() * 1000 ){
-                            if( originaltimestamp < s.getLunchStart().toSecondOfDay() * 1000 && s.getLunchStop().toSecondOfDay() * 1000 < originaltimestamp ){
-                            adjustedtimestamp = originaltimestamp;
-                            adjustmenttype = "Interval Round";
-                            }
-                        }       // UNFINISHED
+                    // DOCK 15 min (CLOCK IN too late to fall within grace period; adjust punch FORWARD in time from start of shift)
+                    else if((lt.isAfter(s.getStart())) && (lt.isBefore(s.getLunchStart())) && (Math.abs(lt.toSecondOfDay() - s.getStart().toSecondOfDay())) > s.getGracePeriod() * 60 && (Math.abs(s.getStart().toSecondOfDay() - lt.toSecondOfDay()) <= s.getDock() * 60)){
+                        adjustedtimestamp = (originaltimestamp - (Math.abs(lt.toSecondOfDay() - s.getStart().toSecondOfDay())) * 1000) + (s.getDock() * 60 * 1000);
+                        adjustmenttype = "Shift Dock";
                     }
-                }
-                
-                // grace period
-                if( strDate != "SAT" && strDate != "SUN" ){
-                    if( s.getStart().toSecondOfDay() * 1000 < originaltimestamp && originaltimestamp < s.getStart().plusMinutes(s.getGracePeriod()).toSecondOfDay() * 1000 ){
-                    adjustedtimestamp = s.getStart().toSecondOfDay() * 1000;
-                    adjustmenttype = "Grace Period";
-                    }
-                }
-                
-                // dock
-                if( strDate != "SAT" && strDate != "SUN" ){
-                    if( s.getStart().plusMinutes(s.getGracePeriod()).toSecondOfDay() * 1000 < originaltimestamp && originaltimestamp < s.getStart().plusMinutes(s.getDock()).toSecondOfDay() * 1000 ){
-                    adjustedtimestamp = originaltimestamp + (s.getDock() * 60000);
-                    adjustmenttype = "Dock";
-                    }
-                }
-                
-                // none
-                if( strDate != "SAT" && strDate != "SUN" ){
-                    if( s.getStart().minusMinutes(1).toSecondOfDay() * 1000 < originaltimestamp && originaltimestamp < s.getStart().plusMinutes(1).toSecondOfDay() * 1000 ){
-                    adjustedtimestamp = s.getStart().toSecondOfDay() * 1000;
-                    adjustmenttype = "None";
-                    }
-                }
+                    // none: (if punch occurred at even time interval, no adjustment needed; reset seconds to zero
+                    else if ((lt.getMinute() % s.getInterval()) == 0 ){
+                        adjustedtimestamp = originaltimestamp - (lt.getSecond() * 1000);
+                        adjustmenttype = "None";
+                    } 
+                    // INTERVAL ROUNDING applies only if OUTSIDE any of the Shift Rules 
+                    else{
+                     intervalRound(lt, s);
+                    }            
+                }                
+                // WEEKEND REQUIRES ONLY INTERVAL ROUNDING
+                else if( strDate == "SAT" || strDate == "SUN" ){
+                     intervalRound(lt, s);
+                }              
+                break;                
+            /************************* TIME OUT *************************/
+            default: 
+                System.err.println("DISREGARD FOR NOW; SEE FEATURE 4");
                 break;
-                
+        }                
+    }
+     
+    //helper method for INTERVAL ROUNDING
+    private void intervalRound(LocalTime lt, Shift s){ // THE MATH IS CORRECT 
+        /* Round up or down to nearest increment of the interval */
+        long mod =  lt.getMinute() % s.getInterval();
+        int halfInterval = s.getInterval()/2;
+        if (mod != 0){
+            if (mod < halfInterval ){ // round DOWN 
+                adjustedtimestamp = originaltimestamp  - (mod*60*1000);
+            }
+            else if (mod >= halfInterval ){ // round UP 
+                adjustedtimestamp = originaltimestamp  + ((s.getInterval() - mod)*60*1000);
+            }
+            adjustedtimestamp = adjustedtimestamp - (lt.getSecond() * 1000);   // adjust seconds to zero            
+            adjustmenttype = "Interval Round";   
         }
     }
-    
+        
     public String printAdjustedTimestamp(){
         StringBuilder output = new StringBuilder();
-        Date date = new Date(originaltimestamp);
+        Date date = new Date(adjustedtimestamp);
 
         SimpleDateFormat formatter = new SimpleDateFormat("EEE MM/dd/yyyy HH:mm:ss");
         String strDate = formatter.format(date);
@@ -237,7 +234,7 @@ public class Punch {
                 break;
         }
  
-        output.append(strDate.toUpperCase()).append("(").append(adjustmenttype).append(")");
+        output.append(strDate.toUpperCase()).append(" ").append("(").append(adjustmenttype).append(")");
             
         return output.toString();
     }
